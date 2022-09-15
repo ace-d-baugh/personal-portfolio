8ff65aaaa22ee1617477fb92a40d87e348467815
Ace Baugh <ace.d.baugh@gmail.com>
2022-09-15 12:10:36 -0400

    Created project card template

--- partials/_projects.js
+++ partials/_projects.js
@@ -4,7 +4,77 @@ class Projects extends HTMLElement {
 	}
 
 	connectedCallback() {
-		this.innerHTML = `<div class="projects" id="projects">PROJECTS</div>`;
+		let imgSrc = 'public/images/17448828_coding.jpg';
+		let projectTitle = 'Project Title';
+		let projectDescription =
+			'Project Description Project Description Project Description Project Description Project Description Project Description ';
+		let techList = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS', 'Express', 'MongoDB'];
+		let projectView = 'https://ace-d-baugh.github.io/personal-portfolio/';
+		let projectCode = 'https://github.com/ace-d-baugh/personal-portfolio';
+
+      function getIcon(tech) {
+         if (tech === 'HTML') {
+            return 'fab fa-html5';
+         } else if (tech === 'CSS') {
+            return 'fab fa-css3-alt';
+         } else if (tech === 'JavaScript') {
+            return 'fab fa-js-square';
+         } else if (tech === 'React') {
+            return 'fab fa-react';
+         } else if (tech === 'NodeJS') {
+            return 'fab fa-node-js';
+         } else if (tech === 'Express') {
+            return 'fas fa-server';
+         } else if (tech === 'MongoDB') {
+            return 'fas fa-database';
+         }
+      }
+
+      function getTechUsed() {
+         let techUsed = '';
+         for (let tech of techList) {
+            techUsed += `<div class="tech-icon">
+                           <i class="${getIcon(tech)}"></i>
+                           <p class="tech-name">${tech}</p>
+                        </div>`;
+         }
+         return techUsed;
+      }
+
+		this.innerHTML = `
+			<div class="row">
+				<div class="project-container center">
+					<div class="project-card-holder">
+						<div class="project-card-image"><img src="${imgSrc}"></div>
+						<div class="project-card">
+							<div class="card-front">
+								<div class="project-title">
+									<h2>${projectTitle}</h2>
+								</div>
+								<div class="project-description">
+									<p>${projectDescription}</p>
+								</div>
+							</div>
+							<div class="card-back">
+                        <div class="project-title">
+									<h2>${projectTitle}</h2>
+								</div>
+                        <div class="tech-used">
+                           <h5>Technologies Used</h5>
+                           <div class="row center">
+                              ${getTechUsed()}
+                           </div>
+                        </div>
+								<div class="card-links">
+									<a href="${projectView}" target='_blank'><div class="btn btn-primary">View Project</div></a>
+									<a href="${projectCode}" target='_blank'><div class="btn btn-primary">View Code</div></a>
+								</div>
+                     </div>
+						</div>
+					</div>
+				</div>
+			</div>
+`;
 	}
 }
 
