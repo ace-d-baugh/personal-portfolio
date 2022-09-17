ae0a98c7ae0c03a8edf14b2e38e6e6f4eb0f13a5
Ace Baugh <ace.d.baugh@gmail.com>
2022-09-17 08:26:30 -0400

    Projects Page Done! Needs updates as time goes on.

--- partials/_footer.js
+++ partials/_footer.js
@@ -9,9 +9,9 @@ class Footer extends HTMLElement {
 		this.innerHTML = `<div class="footer col-12">
       <div class="footer-links col-12">
       <div class="footer-link"><a href="https://github.com/ace-d-baugh" target="_blank">My GitHub</a></div>
-      <div class="footer-link"><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/">Bellevue University's Web Development Degree</a></div>
-      <div class="footer-link"><a href="https://www.youtube.com/user/bellevueuniversity">YouTube</a></div>
-      <div class="footer-link"><a href="https://github.com/buwebdev">Bellevue University's Web Dev GitHub</a></div>
+      <div class="footer-link"><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue University's Web Development Degree</a></div>
+      <div class="footer-link"><a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">YouTube</a></div>
+      <div class="footer-link"><a href="https://github.com/buwebdev" target="_blank">Bellevue University's Web Dev GitHub</a></div>
       </div>
       <div class="copyright">&copy; ${year} All Rights Reserved | Ace Baugh</div>
    </div>`;
