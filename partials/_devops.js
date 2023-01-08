f25df0060d5f87f1af12ea10edfd203a46f47755
Ace Baugh <ace.d.baugh@gmail.com>
2023-01-08 18:04:25 -0500

    Added link to week 1 pres.

--- partials/_devops.js
+++ partials/_devops.js
@@ -14,10 +14,10 @@ class DevOps extends HTMLElement {
 	}
 
 	connectedCallback() {
-      // function that opens a modal with the video presentation
-      function openModal(presentLink) {
-         // build the modal
-         let modal = `
+		// function that opens a modal with the video presentation
+		function openModal(presentLink) {
+			// build the modal
+			let modal = `
             <div class="modal">
                <div class="modal-content">
                   <div class="modal-header">
@@ -25,32 +25,31 @@ class DevOps extends HTMLElement {
                      <h2>DevOps Presentation</h2>
                   </div>
                   <div class="modal-body">
-                     <iframe width="560" height="315" src="${presentLink}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                     <iframe width="560" height="315" src="${presentLink}" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                   </div>
                </div>
             </div>
          `;
-         // output the modal
-         document.getElementById('modal').innerHTML = modal;
-         // get the modal
-         let modalWindow = document.querySelector('.modal');
-         // get the close button
-         let closeBtn = document.querySelector('.close');
-         // open the modal
-         modalWindow.style.display = 'block';
-         // close the modal
-         closeBtn.onclick = function() {
-            modalWindow.style.display = 'none';
-         }
-         // close the modal if the user clicks outside of it
-         window.onclick = function(event) {
-            if (event.target == modalWindow) {
-               modalWindow.style.display = 'none';
-            }
-         };
-      }
+			// output the modal
+			document.getElementById('modal').innerHTML = modal;
+			// get the modal
+			let modalWindow = document.querySelector('#modal');
+			// get the close button
+			let closeBtn = document.querySelector('.close');
+			// open the modal
+			modalWindow.style.display = 'flex';
+			// close the modal
+			closeBtn.onclick = function () {
+				modalWindow.style.display = 'none';
+			};
+			// close the modal if the user clicks outside of it
+			window.onclick = function (event) {
+				if (event.target == modalWindow) {
+					modalWindow.style.display = 'none';
+				}
+			};
+		}
 
-      
 		// Creates the project cards from the project list
 		function getPresents() {
 			let presentCards = '';
@@ -63,37 +62,33 @@ class DevOps extends HTMLElement {
 					// loop through the projects list and creates the project cards
 					for (let present of data) {
 						presentCards += `
-                  <div class="project-card-holder">
-                     <div class="project-card-image"><img src="${present.presentImg}"></div>
-                     <div class="project-card">
-                        <div class="card-front">
-                           <div class="project-title">
-                              <h2>${present.presentTitle}</h2>
-                           </div>
-                           <div class="project-description">
-                              <p>${present.presentDescription}</p>
-                           </div>
-                           <div class="tapForMore">Tap for more</div>
-                        </div>
-                        <div class="card-back">
-                           <div class="project-title">
-                              <h2>${present.presentTitle}</h2>
-                           </div>
-                           <div class="play">
-                              <h5>Play the Presentation</h5>
-                              <div class="play-btn row center">
-                                 <div class="btn btn-primary" onclick="openModal('${present.presentLink}')">Play</div>
+                     <div class="project-card-holder">
+                        <div class="project-card-image"><img src="${present.presentImg}"></div>
+                        <div class="project-card">
+                           <div class="card-front">
+                              <div class="project-title">
+                                 <h2>${present.presentTitle}</h2>
                               </div>
+                              <div class="project-description">
+                                 <p>${present.presentDescription}</p>
+                              </div>
+                              <div class="tapForMore">Tap for more</div>
                            </div>
-                           <div class="card-links">
-                              <a href="${present.presentView}" target='_blank'><div class="btn btn-primary">View Project</div></a>
-                              <a href="${present.presentCode}" target='_blank'><div class="btn btn-primary">View Code</div></a>
+                           <div class="card-back">
+                              <div class="project-title">
+                                 <h2>${present.presentTitle}</h2>
+                              </div>
+                              <div class="play">
+                                 <h5>Play the Presentation</h5>
+                                 <div class="play-btn row center">
+                                    <!-- <div class="btn btn-primary" onclick="openModal("help")">Play</div> -->
+                                    <a href="${present.presentLink}" target="_blank"><div class="btn btn-primary">Play</div></a>
+                                 </div>
+                              </div>
                            </div>
                         </div>
                      </div>
-                  </div>
-               `;
-						idNum++;
+                  `;
 					}
 					// output the project cards
 					document.getElementById('project-container').innerHTML = presentCards;
@@ -104,12 +99,14 @@ class DevOps extends HTMLElement {
 		// Creates the projects holder and calls the getProjects function
 		this.innerHTML = `
 			<div class="projects row" id="projects">
-            <h1 class="section-header">Projects</h1>
+            <h1 class="section-header">DevOps Presentations</h1>
 				<div class="project-container center" id="project-container">
                ${getPresents()}
 				</div>
 			</div>
-`;
+         <!-- <div id="modal" style="display:none;"> -->
+         </div>
+      `;
 	}
 }
 
